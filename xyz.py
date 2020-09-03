f2e7771aa07165a40487c96cda1224cbf40d8ec6
phani-1995 <phanindrasrys@gmail.com>
2020-09-04 00:55:52 +0530

    Added:toss

--- xyz.py
+++ xyz.py
@@ -8,6 +8,8 @@ def select_char():
     chars=('X','O')
     if random.randint(0,1) == 0:
         return chars[::-1]
+        return chars[::-1]
     return chars
-
+player, computer = select_char()
+print('Player is [%s] and computer is [%s]' % (player, computer))
 print("-----------------------Start the Game-------------------------")
