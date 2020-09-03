142265c6a20e3d3ec08bf71e8511e68daf491c27
phani-1995 <phanindrasrys@gmail.com>
2020-09-04 01:02:00 +0530

    Added:printboard

--- xyz.py
+++ xyz.py
@@ -10,6 +10,25 @@ def select_char():
         return chars[::-1]
         return chars[::-1]
     return chars
+
+
+# Table
+tab = range(1, 10)
+
+
+def print_board():
+    x = 1
+    for i in board:
+        end = ' | '
+        if x % 3 == 0:
+            end = ' \n'
+            if i != 1: end += '---------\n';
+        char = ' '
+        if i in ('X', 'O'): char = i;
+        x += 1
+        print(char, end=end)
+print_board()
+
 player, computer = select_char()
 print('Player is [%s] and computer is [%s]' % (player, computer))
 print("-----------------------Start the Game-------------------------")
