2ae9e24b47fe0f5326d3fff0b79405b00e83e406
phani-1995 <phanindrasrys@gmail.com>
2020-09-04 00:43:27 +0530

    Added:Assign-letter

--- xyz.py
+++ xyz.py
@@ -1,3 +1,14 @@
+import random
+import sys
+
+board=[i for i in range(0,9)]
+player, computer = '',''
+
+
+def select_char():
+    chars=('X','O')
+    if random.randint(0,1) == 0:
+        return chars[::-1]
+    return chars
 
 print("-----------------------Start the Game-------------------------")
-
