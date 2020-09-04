50e0b628a788dd497c5ea121dfea46f4c761c1c8
phani-1995 <phanindrasrys@gmail.com>
2020-09-04 11:20:41 +0530

    determine win or loss after every move

--- xyz.py
+++ xyz.py
@@ -4,13 +4,46 @@ import sys
 board=[i for i in range(0,9)]
 player, computer = '',''
 
+# Corners, Center and Others, respectively
+moves=((1,7,3,9),(5,),(2,4,6,8))
+# Winner combinations
+winners=((0,1,2),(3,4,5),(6,7,8),(0,3,6),(1,4,7),(2,5,8),(0,4,8),(2,4,6))
+
+
 def select_char():
     chars=('X','O')
     if random.randint(0,1) == 0:
         return chars[::-1]
         return chars[::-1]
     return chars
-
+def can_move(brd, player, move):
+    if move in tab and brd[move-1] == move-1:
+        return True
+    return False
+def can_win(brd, player, move):
+    places=[]
+    x=0
+    for i in brd:
+        if i == player: places.append(x);
+        x+=1
+    win=True
+    for tup in winners:
+        win=True
+        for ix in tup:
+            if brd[ix] != player:
+                win=False
+                break
+        if win == True:
+            break
+    return win
+def make_move(brd, player, move, undo=False):
+    if can_move(brd, player, move):
+        brd[move-1] = player
+        win=can_win(brd, player, move)
+        if undo:
+            brd[move-1] = move-1
+        return (True, win)
+    return (False, False)
 
 # Table
 tab = range(1, 10)
@@ -22,9 +55,11 @@ def print_board():
         end = ' | '
         if x % 3 == 0:
             end = ' \n'
-            if i != 1: end += '---------\n';
+            if i != 1:
+                end += '---------\n'
         char = ' '
-        if i in ('X', 'O'): char = i;
+        if i in ('X', 'O'):
+            char = i;
         x += 1
         print(char, end=end)
 print_board()
